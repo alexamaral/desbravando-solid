8b387012dbca078718ed6d9ff372ebdc84e81c48
Alex Amaral <alexamaral01@gmail.com>
2024-08-27 14:34:58 -0300

    Capitulo 2 completado

---
+++ cotuba/src/test/cotuba/MainTest.java
@@ -0,0 +1,87 @@
+package cotuba;
+
+import com.github.stefanbirkner.systemlambda.SystemLambda;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    private static Path PDF_BOOK    = Path.of("book.pdf");
+    private static Path EPUB_BOOK   = Path.of("book.epub");
+    private static Path OUTPUT_PDF_BOOK   = Path.of("output.pdf");
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+
+        Files.deleteIfExists(PDF_BOOK);
+        Files.deleteIfExists(EPUB_BOOK);
+        Files.deleteIfExists(OUTPUT_PDF_BOOK);
+    }
+
+    @Test
+    void shoulConvertToPdf(){
+
+        var parameters = new String[]{"-f", "pdf","-d", "livro-exemplo"};
+
+        Main.main(parameters);
+
+        assertTrue(Files.exists(PDF_BOOK));
+
+    }
+
+    @Test
+    void shoulConvertToEpub(){
+
+        var parameters = new String[]{"-f", "epub","-d", "livro-exemplo"};
+
+        Main.main(parameters);
+
+        assertTrue(Files.exists(EPUB_BOOK));
+
+    }
+
+    @Test
+    void shoulConvertToPdfWithVerboseMode(){
+
+        var parameters = new String[]{"-f", "pdf","-d", "livro-exemplo", "-v"};
+
+        Main.main(parameters);
+
+        assertTrue(Files.exists(PDF_BOOK));
+    }
+
+
+    @Test
+    void shoulConvertToPdfWithOutpufile(){
+
+        var parameters = new String[]{"-f", "epub","-d", "livro-exemplo", "-o", "output.pdf"};
+
+        Main.main(parameters);
+
+        assertTrue(Files.exists(OUTPUT_PDF_BOOK));
+    }
+
+    @Test
+    void shoulConvertToPdfWithOutParameters(){
+
+        var parameters = new String[]{"-d", "livro-exemplo"};
+
+        Main.main(parameters);
+
+        assertTrue(Files.exists(PDF_BOOK));
+    }
+
+}
